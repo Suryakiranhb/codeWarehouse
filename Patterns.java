f9cdc5f7ad22204f46378b32b89bec73d63112b5
Suryakiranhb <69113686+Suryakiranhb@users.noreply.github.com>
2021-12-17 11:18:11 +0530

    [12345] x5 pattern
    12345
    12345
    12345
    12345
    12345

--- Patterns.java
+++ Patterns.java
@@ -3,8 +3,8 @@ package com.practice.methods.gcd;
 public class Patterns {
 	public static void main(String[] args) {
 		for (int i=1; i<=5; i++) {
-			for(int j=0; j<=4; j++) {
-				System.out.print(i+ " ");
+			for(int j=1; j<=5; j++) {
+				System.out.print(j+ " ");
 			}
 			System.out.println();
 		}
