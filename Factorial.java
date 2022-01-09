4e9fbc03b562337f898348683f50ad1b7e519bef
Suryakiranhb <69113686+Suryakiranhb@users.noreply.github.com>
2022-01-09 10:10:39 +0530

    Factorial of a number (no recursion)
    Has both decremental (like we usually learn in math) as well as incremental styles.

--- Factorial.java
+++ Factorial.java
@@ -1,29 +1,24 @@
+package com.practice.methods.gcd;
+
 import java.util.Scanner;
 
-class Factorial {
+public class Factorial {
 	public static void main(String[] args) {
-		int fact = 1;
-		System.out.println("Do you want a while loop or a for loop? Enter w or f for the same.");
 		Scanner sc = new Scanner(System.in);
-		char choice = sc.next().charAt(0);
-		
-		System.out.println("Enter the number you wanna find the factorial of: ");
+		System.out.println("Enter the factorial of the number you want to find: ");
 		int num = sc.nextInt();
+//		int ans = 1;
+		int ans=1;
 		
-		if(choice=='f') {
-			for (int i=1; i<=num; i++) {
-				fact = fact*i;
-			}
-			System.out.println("The factorial of the number "+num+" using for loop is "+fact);
+//DECREMENTAL STYLE
+		for(int i=num; i>0; i--) {
+			ans = ans*i;
 		}
-		else if (choice == 'w') {
-				int i = 1;
-				while (i<=num) {
-					fact=fact*i;
-					i++;
-				}
-				System.out.println("The factorial of the number "+num+" using while loop is "+fact);
-				
-		}
+//INCREMENTAL STYLE
+//		for(int i=1; i<=num; i++) {
+//			j = j*i;
+//		}
+		
+		System.out.println("The factorial is "+ans);
 	}
 }
