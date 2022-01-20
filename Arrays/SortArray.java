0f7322dbbee01a5628540084ea9d8630c995eac1
Suryakiranhb <69113686+Suryakiranhb@users.noreply.github.com>
2022-01-20 15:43:20 +0530

    Sorts the array using bubble sort

---
+++ Arrays/SortArray.java
@@ -0,0 +1,30 @@
+package com.practice.tekstac.arrays;
+
+import java.util.Scanner;
+
+public class SortArray {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the array size");
+		int size = sc.nextInt();
+		int[] a = new int[size];
+		System.out.println("Enter the values");
+		for(int i=0; i<=size-1; i++) {
+			a[i] = sc.nextInt();
+		}
+		//sorting using bubble sort
+		for(int i=0; i<=size-1; i++) {
+			for(int j=i+1; j<=size-1; j++) {
+				if(a[j]<a[i]) {
+					int temp = a[j];
+					a[j] = a[i];
+					a[i] = temp;
+				}
+			}
+		}
+		//displaying sorted array
+		for(int i=0; i<=size-1; i++) {
+			System.out.println(a[i]);
+		}
+	}
+}
