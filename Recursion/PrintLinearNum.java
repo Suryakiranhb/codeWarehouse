11916c56b69530547b319e95ce4a48a3c7de8a3d
Suryakiranhb <69113686+Suryakiranhb@users.noreply.github.com>
2022-12-12 12:22:58 +0530

    Print numbers linearly
    Prints n numbers linearly using recursion

---
+++ Recursion/PrintLinearNum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class PrintLinearNum {
+		public static void rec(int start, int i) {
+			if(start == i+1) {
+				return;
+			}
+			else {
+				System.out.println(start);
+				rec(start+=1, i);
+			}
+			
+		}
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter number ");
+			int num = sc.nextInt();
+			rec(1,num);
+			sc.close();
+	}
+}
