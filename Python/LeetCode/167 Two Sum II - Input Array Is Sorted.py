d8b2d8ea0c25d388b3c68d1e918c63dc7c8e7808
Suryakiranhb <69113686+Suryakiranhb@users.noreply.github.com>
2025-08-10 19:37:51 +0530

    167 Two current_sum II - Input Array Is Sorted

---
+++ Python/LeetCode/167 Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,33 @@
+# 167. Two current_sum II - Input Array Is Sorted
+
+# brute force 
+def two_current_sum(numbers, target):
+    pointer1 = 0
+    pointer2 = len(numbers)-1
+    for i in range(0, len(numbers)):
+        for j in range(i,len(numbers)):
+            if numbers[i] + numbers[j] == target:
+                return i+1,j+1
+
+
+#2 pointer
+def two_current_sum_2pointer(numbers,target):
+    pointer1 = 0
+    pointer2 = len(numbers)-1
+    while(pointer1<pointer2 and pointer1,pointer2<len(numbers)):
+        current_sum = numbers[pointer1]+numbers[pointer2]
+        print("pointer1: ",pointer1, " Pointer2: ",pointer2, " current_sum = ",current_sum)
+        if current_sum == target:
+            return pointer1+1, pointer2+1
+        elif current_sum>target:
+            pointer2-=1
+        elif current_sum<target:
+            pointer1+=1
+    return -1
+
+
+
+numbers = [2,7,11,15]
+target = 9
+pointer1, pointer2  = two_current_sum_2pointer(numbers, target)
+print(f"Answer is to add numbers {numbers[pointer1]} and {numbers[pointer2]}")
